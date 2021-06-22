5389b02722e936aea6b16b0872e6ddcaa9513726
Jenna Anderson <jennaanderson@Jennas-MacBook-Air.local>
2021-06-22 09:43:34 -0600

    recreated nav to route to home, sign up, sign in, and classes components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { Link, Route, Switch } from 'react-router-dom'
+import { AppBar, Tabs, Tab, TabPanel } from '@material-ui/core'
 
 import SignUp from './components/SignUp';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+       <AppBar position="static">
+        <Tabs /*onChange={handleChange}*/ aria-label="simple tabs example" centered>
+        <Link to='/' className="navLink"><Tab label="Home"/></Link>
+        <Link to='/sign-up' className="navLink"><Tab label="Sign Up"  /></Link>
+        <Link to='/sign-in' className="navLink"><Tab label="Sign In"  /></Link>
+        <Link to='/classes' className="navLink"><Tab label="Classes"  /></Link>
+        </Tabs>
+      </AppBar>
+      {/* <header className="App-header">
         <h1>Anywhere Fitness</h1>
-      </header>
-      <SignUp/>
+      </header> */}
+
+      <Switch>
+        <Route exact path='/'>
+          {/* Home page component */}
+        </Route>
+
+        <Route exact path='/sign-up'>
+          <SignUp />
+        </Route>
+
+        <Route exact path='sign-in'>
+          {/* Sign in component */}
+        </Route>
+
+        <Route exact path='/classes'>
+          {/* Classes component - only shown when logged in */}
+        </Route>
+
+      </Switch>
+
     </div>
   );
 }
