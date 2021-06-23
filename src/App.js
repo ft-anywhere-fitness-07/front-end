222fc4cb4178788db29cce3b51fae9fdf66f1dc3
Jenna Anderson <jennaanderson@Jennas-MacBook-Air.local>
2021-06-23 11:26:09 -0600

    removed intitial mock classes from classList

--- src/App.js
+++ src/App.js
@@ -2,35 +2,11 @@ import './App.css';
 import React, { useState } from 'react';
 import NavBar from './components/NavBar';
 
-const initialClasses = [
-  {
-    classId: 0,
-    name: "Stand Up PaddleBoard Yoga",
-    type: "Yoga",
-    time: "8:00",
-    duration: "60 min",
-    intensityLvl: 3,
-    attendees:6,
-    maxSize: 20,
-    location: "Loveland, CO",
-  },
-  {
-    classId: 1,
-    name: "Long Run",
-    type: "Running",
-    time: "7:00",
-    duration: "90 min",
-    intensityLvl: 7,
-    attendees: 5,
-    maxSize: 40,
-    location: "Estes Park, CO",
-  }
-]
 
 function App() {
   const [isAuth, setIsAuth] = useState(false)
   const [isInstructor, setIsInstructor] = useState(false);
-  const [classList, setClassList] = useState(initialClasses);
+  const [classList, setClassList] = useState([]);
   console.log(isInstructor)
 
   return (
