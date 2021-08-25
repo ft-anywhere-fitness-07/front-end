35dcb0b4fc6260792b3f650bde04ec1c412c85ce
jenna-anderson <jenna.d.anderson@gmail.com>
2021-08-25 15:06:59 -0600

    removes logger middleware

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import rootReducer from './reducers';
@@ -21,7 +21,7 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-const store = createStore(persistedReducer, applyMiddleware(thunk, logger))
+const store = createStore(persistedReducer, applyMiddleware(thunk))
 const persistor = persistStore(store)
 
 ReactDOM.render(
